22ffc275c806bdb056d716b5abe43c9921055ae8
Dhaval Birade <69907022+den864@users.noreply.github.com>
2025-09-09 08:48:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+
+document.querySelector("form").addEventListener("submit",(event)=>{
+    event.preventDefault();
+
+    const form=document.querySelector("form");
+    const data=new FormData(form);
+
+    const fname=data.get("fname").trim();
+    const lname=data.get("lname").trim();
+    const phone=data.get("phone_number").trim();
+    const email=data.get("email").trim();
+
+    if(!fname || !lname || !phone || !email) return;
+
+    const s=`First Name: ${fname} Last Name: ${lname} Phone Number: ${phone} Email ID: ${email}`;
+    alert(s);
+
+})
